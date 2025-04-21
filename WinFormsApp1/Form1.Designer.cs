68107d8b26a026a70cb13dab6c454cc2df552f19
Munguntulg <moogii2032@gmail.com>
2025-04-21 17:32:32 +0800

    Add

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Nemeh = new Button();
+            hasah = new Button();
+            hariu = new Label();
+            Tentsuu = new Button();
+            Memory = new Button();
+            ys = new Button();
+            guraw = new Button();
+            dorow = new Button();
+            taw = new Button();
+            zurgaa = new Button();
+            doloo = new Button();
+            naim = new Button();
+            neg = new Button();
+            hoyr = new Button();
+            teg = new Button();
+            tegteg = new Button();
+            SuspendLayout();
+            // 
+            // Nemeh
+            // 
+            Nemeh.Font = new Font("Segoe UI", 13F);
+            Nemeh.Location = new Point(691, 292);
+            Nemeh.Name = "Nemeh";
+            Nemeh.Size = new Size(75, 37);
+            Nemeh.TabIndex = 0;
+            Nemeh.Text = "+";
+            Nemeh.UseVisualStyleBackColor = true;
+            Nemeh.Click += Nemeh_Click;
+            // 
+            // hasah
+            // 
+            hasah.Font = new Font("Segoe UI", 13F);
+            hasah.Location = new Point(691, 335);
+            hasah.Name = "hasah";
+            hasah.Size = new Size(75, 39);
+            hasah.TabIndex = 1;
+            hasah.Text = "-";
+            hasah.UseVisualStyleBackColor = true;
+            hasah.Click += hasah_Click;
+            // 
+            // hariu
+            // 
+            hariu.AutoSize = true;
+            hariu.Font = new Font("Segoe UI", 30F);
+            hariu.Location = new Point(513, 74);
+            hariu.Name = "hariu";
+            hariu.Size = new Size(253, 54);
+            hariu.TabIndex = 2;
+            hariu.Text = "ur dun garna";
+            hariu.Click += Hariu;
+            // 
+            // Tentsuu
+            // 
+            Tentsuu.Location = new Point(691, 380);
+            Tentsuu.Name = "Tentsuu";
+            Tentsuu.Size = new Size(75, 35);
+            Tentsuu.TabIndex = 14;
+            Tentsuu.Text = "=";
+            Tentsuu.Click += Tentsuu_Click;
+            // 
+            // Memory
+            // 
+            Memory.Location = new Point(691, 248);
+            Memory.Name = "Memory";
+            Memory.Size = new Size(75, 38);
+            Memory.TabIndex = 13;
+            Memory.Text = "M";
+            Memory.UseVisualStyleBackColor = true;
+            Memory.Click += Memory_Click;
+            // 
+            // ys
+            // 
+            ys.Font = new Font("Segoe UI", 13F);
+            ys.Location = new Point(585, 249);
+            ys.Name = "ys";
+            ys.Size = new Size(75, 37);
+            ys.TabIndex = 15;
+            ys.Text = "9";
+            ys.UseVisualStyleBackColor = true;
+            ys.Click += ys_Click;
+            // 
+            // guraw
+            // 
+            guraw.Font = new Font("Segoe UI", 13F);
+            guraw.Location = new Point(585, 335);
+            guraw.Name = "guraw";
+            guraw.Size = new Size(75, 37);
+            guraw.TabIndex = 16;
+            guraw.Text = "3";
+            guraw.UseVisualStyleBackColor = true;
+            guraw.Click += guraw_Click;
+            // 
+            // dorow
+            // 
+            dorow.Font = new Font("Segoe UI", 13F);
+            dorow.Location = new Point(423, 292);
+            dorow.Name = "dorow";
+            dorow.Size = new Size(75, 37);
+            dorow.TabIndex = 17;
+            dorow.Text = "4";
+            dorow.UseVisualStyleBackColor = true;
+            dorow.Click += dorow_Click;
+            // 
+            // taw
+            // 
+            taw.Font = new Font("Segoe UI", 13F);
+            taw.Location = new Point(504, 292);
+            taw.Name = "taw";
+            taw.Size = new Size(75, 37);
+            taw.TabIndex = 18;
+            taw.Text = "5";
+            taw.UseVisualStyleBackColor = true;
+            taw.Click += taw_Click;
+            // 
+            // zurgaa
+            // 
+            zurgaa.Font = new Font("Segoe UI", 13F);
+            zurgaa.Location = new Point(585, 292);
+            zurgaa.Name = "zurgaa";
+            zurgaa.Size = new Size(75, 37);
+            zurgaa.TabIndex = 19;
+            zurgaa.Text = "6";
+            zurgaa.UseVisualStyleBackColor = true;
+            zurgaa.Click += zurgaa_Click;
+            // 
+            // doloo
+            // 
+            doloo.Font = new Font("Segoe UI", 13F);
+            doloo.Location = new Point(423, 250);
+            doloo.Name = "doloo";
+            doloo.Size = new Size(75, 37);
+            doloo.TabIndex = 20;
+            doloo.Text = "7";
+            doloo.UseVisualStyleBackColor = true;
+            doloo.Click += doloo_Click;
+            // 
+            // naim
+            // 
+            naim.Font = new Font("Segoe UI", 13F);
+            naim.Location = new Point(504, 249);
+            naim.Name = "naim";
+            naim.Size = new Size(75, 37);
+            naim.TabIndex = 21;
+            naim.Text = "8";
+            naim.UseVisualStyleBackColor = true;
+            naim.Click += naim_Click;
+            // 
+            // neg
+            // 
+            neg.Font = new Font("Segoe UI", 13F);
+            neg.Location = new Point(423, 336);
+            neg.Name = "neg";
+            neg.Size = new Size(75, 37);
+            neg.TabIndex = 22;
+            neg.Text = "1";
+            neg.UseVisualStyleBackColor = true;
+            neg.Click += neg_Click;
+            // 
+            // hoyr
+            // 
+            hoyr.Font = new Font("Segoe UI", 13F);
+            hoyr.Location = new Point(504, 337);
+            hoyr.Name = "hoyr";
+            hoyr.Size = new Size(75, 37);
+            hoyr.TabIndex = 23;
+            hoyr.Text = "2";
+            hoyr.UseVisualStyleBackColor = true;
+            hoyr.Click += hoyr_Click;
+            // 
+            // teg
+            // 
+            teg.Font = new Font("Segoe UI", 13F);
+            teg.Location = new Point(423, 378);
+            teg.Name = "teg";
+            teg.Size = new Size(75, 37);
+            teg.TabIndex = 24;
+            teg.Text = "0";
+            teg.UseVisualStyleBackColor = true;
+            teg.Click += teg_Click;
+            // 
+            // tegteg
+            // 
+            tegteg.Font = new Font("Segoe UI", 13F);
+            tegteg.Location = new Point(504, 377);
+            tegteg.Name = "tegteg";
+            tegteg.Size = new Size(75, 37);
+            tegteg.TabIndex = 25;
+            tegteg.Text = "00";
+            tegteg.UseVisualStyleBackColor = true;
+            tegteg.Click += tegteg_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tegteg);
+            Controls.Add(teg);
+            Controls.Add(hoyr);
+            Controls.Add(neg);
+            Controls.Add(naim);
+            Controls.Add(doloo);
+            Controls.Add(zurgaa);
+            Controls.Add(taw);
+            Controls.Add(dorow);
+            Controls.Add(guraw);
+            Controls.Add(ys);
+            Controls.Add(Memory);
+            Controls.Add(Tentsuu);
+            Controls.Add(hariu);
+            Controls.Add(hasah);
+            Controls.Add(Nemeh);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Nemeh;
+        private Button hasah;
+        private Label hariu;
+        private Button Tentsuu;
+        private Button Memory;
+        private Button ys;
+        private Button guraw;
+        private Button dorow;
+        private Button taw;
+        private Button zurgaa;
+        private Button doloo;
+        private Button naim;
+        private Button neg;
+        private Button hoyr;
+        private Button teg;
+        private Button tegteg;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+using TooniiMachine;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Nemeh_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Tsipruud_Click(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void Hariu(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Memory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hasah_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tentsuu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zurgaa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void neg_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hoyr_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guraw_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dorow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void taw_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doloo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void naim_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ys_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void teg_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tegteg_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
